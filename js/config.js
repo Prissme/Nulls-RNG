1d088103e47a796b194834cef65d6cc47f41e84e
Prissme <drissbouhaiat@gmail.com>
2026-06-18 01:07:45 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+/* Ce fichier est régénéré automatiquement au démarrage du conteneur
+   (voir docker-entrypoint.sh) à partir des variables d'environnement
+   SUPABASE_URL et SUPABASE_ANON_KEY définies dans Koyeb.
+   En local / sans Docker, il reste vide : le jeu fonctionne normalement,
+   simplement sans sauvegarde cloud. */
+window.SUPABASE_URL = "";
+window.SUPABASE_ANON_KEY = "";
