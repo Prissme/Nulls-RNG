090834e112a6e9e40f4956fbc629a34f3eccaf79
Prissme <drissbouhaiat@gmail.com>
2026-06-28 16:28:12 +0200

    Add files via upload

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Injecte les variables d'environnement Koyeb (SUPABASE_URL, SUPABASE_ANON_KEY)
 # dans js/config.js à partir du template, juste avant de démarrer nginx.
-envsubst '${SUPABASE_URL} ${SUPABASE_ANON_KEY}' \
+envsubst '${SUPABASE_URL} ${SUPABASE_ANON_KEY} ${DISCORD_BOT_TOKEN} ${DISCORD_CHANNEL_ID}' \
   < /usr/share/nginx/html/js/config.template.js \
   > /usr/share/nginx/html/js/config.js
 
